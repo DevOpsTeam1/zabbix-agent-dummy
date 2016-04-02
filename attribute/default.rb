8ebdd6a98fc0a1dbcba8c51f25cd66644e3c6407
scherbank <konstantin_scherban@epam.com>
2016-04-02 17:41:22 +0300

    Update default.rb

--- attribute/default.rb
+++ attribute/default.rb
@@ -4,11 +4,7 @@ default['zabbix']['agent']['user']         = 'zabbix'
 default['zabbix']['agent']['group']        = 'zabbix'
 
 default['zabbix']['install_dir']      = '/opt/zabbix'
-default['zabbix']['lock_dir']         = '/var/lock/subsys'
-default['zabbix']['log_dir']          = '/var/log/zabbix'
-default['zabbix']['run_dir']          = '/var/run/zabbix'
-
-default['zabbix']['agent']['scripts'] = '/etc/zabbix/scripts'
+default['zabbix']['scripts']          = '/etc/zabbix/scripts'
 
 default['zabbix']['agent']['version']     = '3.0'
 default['zabbix']['agent']['hostname']    = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'app_bundle' }[0]
