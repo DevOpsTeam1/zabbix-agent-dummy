a655d322a382e63617aa12ab28920aeb7f55ebec
scherbank <konstantin_scherban@epam.com>
2016-04-02 17:17:18 +0300

    Update default.rb

--- attribute/default.rb
+++ attribute/default.rb
@@ -1,1 +1,23 @@
 default['php']['install_method'] = 'source'
+
+default['zabbix']['agent']['user']         = 'zabbix'
+default['zabbix']['agent']['group']        = 'zabbix'
+
+default['zabbix']['install_dir']      = '/opt/zabbix'
+default['zabbix']['lock_dir']         = '/var/lock/subsys'
+default['zabbix']['log_dir']          = '/var/log/zabbix'
+default['zabbix']['run_dir']          = '/var/run/zabbix'
+
+default['zabbix']['agent']['scripts'] = '/etc/zabbix/scripts'
+
+default['zabbix']['agent']['version']     = '3.0'
+default['zabbix']['agent']['hostname']    = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'app-bundle' }[0]
+normal['zabbix']['agent']['servers_ip']   = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'zabbix-srv' }[0]
+normal['zabbix']['agent']['servers']      = normal['zabbix']['agent']['servers_active'] = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'zabbix-srv' }[1]['private_ip_address']
+
+default['zabbix']['agent']['timeout']        = '3'
+default['zabbix']['agent']['listen_port']    = '10050'
+default['zabbix']['agent']['jmx_port']       = '10052'
+
+default['zabbix']['agent']['conf']['LogType']      = 'system'
+default['zabbix']['agent']['conf']['PidFile']      = '/var/run/zabbix/zabbix_agentd.pid'
