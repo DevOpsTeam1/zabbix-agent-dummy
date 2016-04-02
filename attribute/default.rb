110ba3601f56dcc755ec09a0c107e97b1a913952
scherbank <konstantin_scherban@epam.com>
2016-04-02 19:04:30 +0300

    Update default.rb

--- attribute/default.rb
+++ attribute/default.rb
@@ -10,9 +10,10 @@ default['zabbix']['install_dir']      = '/opt/zabbix'
 default['zabbix']['scripts']          = '/etc/zabbix/scripts'
 
 default['zabbix']['agent']['version']     = '3.0'
-default['zabbix']['agent']['hostname']    = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'app-bundle' }[0]
+$#default['zabbix']['agent']['hostname']    = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'app-bundle' }[0]
 normal['zabbix']['agent']['servers_name']   = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'zabbix-srv' }[0]
 normal['zabbix']['agent']['servers_ip']      = normal['zabbix']['agent']['servers_active'] = node['epc-provisioning']['instances'].find { |i| i[1]['role'] == 'zabbix-srv' }[1]['private_ip_address']
+default['zabbix']['agent']['hostname']    = node['fqdn']
 
 default['zabbix']['agent']['timeout']        = '3'
 default['zabbix']['agent']['listen_port']    = '10050'
